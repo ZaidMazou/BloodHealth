80fd7047c560617214a1dde2b6d772ca73a4975d
zaid <zaidmazou@gmail.com>
2024-08-14 02:47:38 +0100

    admin user

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -39,7 +39,7 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
-            'admin'=>0
+            'admin'=>1
         ]);
 
         event(new Registered($user));
